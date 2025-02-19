ea813d3b2d8aebeb43eb3c0ab736a7a66b142b3f
ismail-dev-code <ismail.online0385678@gmail.com>
2025-02-19 19:03:03 +0600

    problem solved 1 to 4 done

---
+++ problem3.js
@@ -0,0 +1,21 @@
+function willSuccess(marks) {
+    if (!Array.isArray(marks)) {
+      return "Invalid";
+    }
+  
+    let passMark = 0;
+    let failMark = 0;
+    for (const mark of marks) {
+      if (mark >= 50) {
+        passMark++;
+      } 
+       else {
+        failMark++;
+      }
+    }
+    return passMark > failMark;
+  }
+  
+  const result = willSuccess("100 , 100");
+  console.log(result);
+  
